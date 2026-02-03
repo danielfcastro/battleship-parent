23d5c4ddc130fb53f36216dbcae46fa705d6b1ee
Daniel F. Castro <dfcastro@gmail.com>
2026-02-03 15:07:49 +0000

    • code deduplication would be over engineering • code coverage report

--- battleship-service/src/main/java/com/odigeo/interview/coding/battleshipservice/controller/EngineeringController.java
+++ battleship-service/src/main/java/com/odigeo/interview/coding/battleshipservice/controller/EngineeringController.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/engineering")
 @Produces(MediaType.TEXT_PLAIN)
+@SuppressWarnings("java:S1192") // Simple health check endpoint duplicated across microservices
 public class EngineeringController {
 
     @GET
--- battleship-service/src/test/java/com/odigeo/interview/coding/battleshipservice/controller/EngineeringControllerTest.java
+++ battleship-service/src/test/java/com/odigeo/interview/coding/battleshipservice/controller/EngineeringControllerTest.java
@@ -3,6 +3,7 @@ package com.odigeo.interview.coding.battleshipservice.controller;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@SuppressWarnings("java:S1192") // Simple health check test duplicated across microservices
 public class EngineeringControllerTest {
 
     EngineeringController engineeringController;
